7ce3dce306aa1758bf2edbcd36f2db62bfbc28b1
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2024-02-08 07:24:03 -0800

    updated functions  to handle the additional requirements for adding roles, adding employees  and  updating their roles, app runs fine

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connection.connect((err) => {
     console.log('Connected to MySQL database');
     startApp();
   });
-  
+
 // Function to start the application
 function startApp() {
     inquirer
@@ -103,5 +103,99 @@ function startApp() {
     startApp();
   }
   
+// Function to add a role
+async function addRole() {
+  const roleInfo = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of the new role:',
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'Enter the salary for the new role:',
+    },
+    {
+      type: 'number',
+      name: 'department_id',
+      message: 'Enter the department ID for the new role:',
+    },
+  ]);
+
+  await query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', [
+    roleInfo.title,
+    roleInfo.salary,
+    roleInfo.department_id,
+  ]);
+
+  console.log('Role added successfully!');
+  startApp();
+}
+
+// Function to add an employee
+async function addEmployee() {
+  const employeeInfo = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'Enter the first name of the new employee:',
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'Enter the last name of the new employee:',
+    },
+    {
+      type: 'number',
+      name: 'role_id',
+      message: 'Enter the role ID for the new employee:',
+    },
+    {
+      type: 'number',
+      name: 'manager_id',
+      message: 'Enter the manager ID for the new employee (if applicable):',
+    },
+  ]);
+
+  await query('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [
+    employeeInfo.first_name,
+    employeeInfo.last_name,
+    employeeInfo.role_id,
+    employeeInfo.manager_id,
+  ]);
+
+  console.log('Employee added successfully!');
+  startApp();
+}
+
+// Function to update an employee's role
+async function updateEmployeeRole() {
+  // Get the list of employees for the user to choose from
+  const employees = await query('SELECT id, CONCAT(first_name, " ", last_name) AS full_name FROM employees');
+  
+  // Prompt the user to select an employee to update
+  const employeeToUpdate = await inquirer.prompt({
+    type: 'list',
+    name: 'employee_id',
+    message: 'Select the employee whose role you want to update:',
+    choices: employees.map((employee) => ({
+      value: employee.id,
+      name: employee.full_name,
+    })),
+  });
+
+  // Prompt the user to enter the new role ID for the selected employee
+  const newRoleId = await inquirer.prompt({
+    type: 'number',
+    name: 'role_id',
+    message: 'Enter the new role ID for the employee:',
+  });
+
+  await query('UPDATE employees SET role_id = ? WHERE id = ?', [newRoleId.role_id, employeeToUpdate.employee_id]);
+
+  console.log('Employee role updated successfully!');
+  startApp();
+}
 
 module.exports = connection;
